larsks/cloudkit-operator

Add nodeCounts field to status
This adds a nodeCounts field to the ClusterOrder status:

    status:
      nodeCounts:
      - resourceClass: foo
        numberOfNodes: 2
      - resourceClass: bar
        numberOfNodes: 1

Closes innabox/issues#117

api/v1alpha1/clusterorder_types.go
@@ -112,6 +112,9 @@ type ClusterOrderStatus struct {
 	// Reference to the namespace that contains the HostedCluster resource
 	// +kubebuilder:validation:Optional
 	ClusterReference *ClusterOrderClusterReferenceType `json:"clusterReference,omitempty"`
+
+	// NodeCount shows how many nodes are currently associated with the ClusterOrder
+	NodeCounts []NodeRequest `json:"nodeCounts,omitempty"`
 }
 
 // +kubebuilder:object:root=true

api/v1alpha1/zz_generated.deepcopy.go
@@ -134,6 +134,11 @@ func (in *ClusterOrderStatus) DeepCopyInto(out *ClusterOrderStatus) {
 		*out = new(ClusterOrderClusterReferenceType)
 		**out = **in
 	}
+	if in.NodeCounts != nil {
+		in, out := &in.NodeCounts, &out.NodeCounts
+		*out = make([]NodeRequest, len(*in))
+		copy(*out, *in)
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ClusterOrderStatus.

jkary/osac

openshift/operator/crds/v1alpha1/clusterorder_types.go
@@ -112,6 +112,9 @@ type ClusterOrderStatus struct {
 	// Reference to the namespace that contains the HostedCluster resource
 	// +kubebuilder:validation:Optional
 	ClusterReference *ClusterOrderClusterReferenceType `json:"clusterReference,omitempty"`
+
+	// NodeRequests reflects how many nodes are currently associated with the ClusterOrder
+	NodeRequests []NodeRequest `json:"nodeCounts,omitempty"`
 }
 
 // +kubebuilder:object:root=true

openshift/operator/crds/v1alpha1/zz_generated.deepcopy.go
@@ -134,6 +134,11 @@ func (in *ClusterOrderStatus) DeepCopyInto(out *ClusterOrderStatus) {
 		*out = new(ClusterOrderClusterReferenceType)
 		**out = **in
 	}
+	if in.NodeRequests != nil {
+		in, out := &in.NodeRequests, &out.NodeRequests
+		*out = make([]NodeRequest, len(*in))
+		copy(*out, *in)
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ClusterOrderStatus.